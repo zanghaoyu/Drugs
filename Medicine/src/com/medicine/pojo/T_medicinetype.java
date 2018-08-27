1cdc092be85ab6a5d89f396178195a3e40ba64d5
Administrator <Administrator@KZ-20180731NKXU>
2018-08-27 16:02:01 +0800

    药品类别

--- Medicine/src/com/medicine/pojo/T_medicinetype.java
+++ Medicine/src/com/medicine/pojo/T_medicinetype.java
@@ -4,12 +4,23 @@ import java.io.Serializable;
 
 public class T_medicinetype implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -2057973602820477616L;
 
-	private  String MedicineTypeId;//药品类别编号
-	private  String Medicinename;//药品类别名称
-		
+	private  String medicineTypeId;//药品类别编号
+	private  String medicinename;//药品类别名称
+	
+	public String getMedicineTypeId() {
+		return medicineTypeId;
+	}
+	public void setMedicineTypeId(String medicineTypeId) {
+		this.medicineTypeId = medicineTypeId;
+	}
+	public String getMedicinename() {
+		return medicinename;
+	}
+	public void setMedicinename(String medicinename) {
+		this.medicinename = medicinename;
+	}
+	
+	
 }
