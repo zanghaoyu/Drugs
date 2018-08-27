1f35c80eb0239278b4ca7c4ea7cdc38a926c123b
Administrator <Administrator@kz201803061651>
2018-08-27 15:17:08 +0800

    MED

---
+++ Medicine/src/com/medicine/pojo/T_medicinetype.java
@@ -0,0 +1,15 @@
+package com.medicine.pojo;
+
+import java.io.Serializable;
+
+public class T_medicinetype implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2057973602820477616L;
+
+	private  String MedicineTypeId;//药品类别编号
+	private  String Medicinename;//药品类别名称
+		
+}
