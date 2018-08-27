a74197b0ab7d74f29f7385750207787bc63a76d3
suiran <420079156@qq.com>
2018-08-27 17:08:45 +0800

    客户实体类

--- Medicine/src/com/medicine/pojo/Medicinetype.java
+++ Medicine/src/com/medicine/pojo/Medicinetype.java
@@ -2,11 +2,12 @@ package com.medicine.pojo;
 
 import java.io.Serializable;
 
-public class T_medicinetype implements Serializable {
+public class Medicinetype implements Serializable {
 
 	private static final long serialVersionUID = -2057973602820477616L;
 
 	private  String medicineTypeId;//药品类别编号
+	
 	private  String medicinename;//药品类别名称
 	
 	public String getMedicineTypeId() {
--- Medicine/src/util/DBUTIL.java
+++ Medicine/src/util/DBUTIL.java
@@ -1,6 +1,5 @@
 package util;
 
-import static org.junit.Assert.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,7 +8,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import org.junit.Test;
 
 public class DBUTIL {
 	
